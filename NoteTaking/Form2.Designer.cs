bc2c3a9d4ce765c4c1546478c6ddbfe5047a592a
themilnera <amilner32@gmail.com>
2025-07-02 13:18:10 -0600

    Add project files.

---
+++ NoteTaking/Form2.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace NoteTaking
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.closeButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitle2 = new System.Windows.Forms.TextBox();
+            this.txtField2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(657, 200);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(87, 39);
+            this.closeButton.TabIndex = 0;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(48, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Title:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(48, 208);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(46, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Text:";
+            // 
+            // txtTitle2
+            // 
+            this.txtTitle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTitle2.Location = new System.Drawing.Point(100, 33);
+            this.txtTitle2.Name = "txtTitle2";
+            this.txtTitle2.Size = new System.Drawing.Size(179, 26);
+            this.txtTitle2.TabIndex = 3;
+            this.txtTitle2.TextChanged += new System.EventHandler(this.txtTitle2_TextChanged);
+            // 
+            // txtField2
+            // 
+            this.txtField2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtField2.Location = new System.Drawing.Point(100, 200);
+            this.txtField2.Multiline = true;
+            this.txtField2.Name = "txtField2";
+            this.txtField2.Size = new System.Drawing.Size(448, 181);
+            this.txtField2.TabIndex = 4;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtField2);
+            this.Controls.Add(this.txtTitle2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.closeButton);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button closeButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitle2;
+        private System.Windows.Forms.TextBox txtField2;
+    }
+}
---
+++ NoteTaking/Form2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace NoteTaking
+{
+    public partial class Form2 : Form
+    {
+        public string displayedText = "";
+        public string displayedTitle = "";
+        public Form2(string displayedText, string displayedTitle)
+        {
+            InitializeComponent();
+            this.displayedText = displayedText;
+            this.displayedTitle = displayedTitle;
+            txtTitle2.Text = displayedTitle;
+            txtField2.Text = displayedText;
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void txtTitle2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
